a7c72b6f8986b4ee9114f739a68afa8923fd790f
Nikki A.A <nikki.asteinza@gmail.com>
2023-07-10 00:20:12 +0200

    AddProductFromList working

--- website/src/app/pages/product-list/components/product/product.component.ts
+++ website/src/app/pages/product-list/components/product/product.component.ts
@@ -8,15 +8,12 @@ import { CartProductI } from 'src/app/core/interfaces/cart-product-interface';
 })
 export class ProductComponent implements OnInit {
   @Input() public product?: CartProductI;
-  @Output() public onRemoveProduct =  new EventEmitter<void>(); // Podemos no pasarle nada, sabe de donde viene el uotput , el padre
-  @Output() public onAddProduct =  new EventEmitter<void>(); 
+  @Output() public onAddProductFromList =  new EventEmitter<void>(); 
   ngOnInit(): void {
 
   }
-  public removeProduct(){
-    this.onRemoveProduct.emit(); // Hay que emitir al padre
-  }
   public addProduct(){
-    this.onAddProduct.emit(); 
+    console.log("add product")
+    this.onAddProductFromList.emit(); 
   }
 }
--- website/src/app/pages/product-list/components/shopping-cart/components/cart-item/cart-item.component.ts
+++ website/src/app/pages/product-list/components/shopping-cart/components/cart-item/cart-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CartProductI } from 'src/app/core/interfaces/cart-product-interface';
 
 @Component({
@@ -8,4 +8,15 @@ import { CartProductI } from 'src/app/core/interfaces/cart-product-interface';
 })
 export class CartItemComponent {
   @Input() public cartProduct?: CartProductI;
+  @Output() public onRemoveProductFromCart =  new EventEmitter<void>(); 
+  @Output() public onAddProductFromCart =  new EventEmitter<void>(); 
+  ngOnInit(): void {
+
+  }
+  public addProduct(){
+    this.onAddProductFromCart.emit(); 
+  }
+  public removeProduct(){
+    this.onRemoveProductFromCart.emit(); 
+  }
 }
