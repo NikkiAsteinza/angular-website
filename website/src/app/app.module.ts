3197a41535a554c4132a77e08e9921b8da085b0d
Nikki <nikki.asteinza@gmail.com>
2023-07-09 11:58:41 +0200

    header + angular material

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListModule } from './pages/product-list/product-list.module';
+import { HeaderComponent } from './core/components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ProductListModule
+    ProductListModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ website/src/app/core/avengers-list.config.ts
@@ -0,0 +1,59 @@
+// Tipar la constante con la interfaz que créeis en una archivo avengers.models.ts
+export const avengers = [
+  {
+    hero: 'Spider-Man',
+    name: 'Peter Benjamin Parker',
+    link: 'http://marvel.com/characters/54/spider-man',
+    img: 'https://terrigen-cdn-dev.marvel.com/content/prod/1x/005smp_ons_crd_02.jpg',
+    isActive: false
+  },
+  {
+    hero: 'Scarlet Witch',
+    name: 'Wanda Maximoff',
+    link: 'https://www.marvel.com/characters/scarlet-witch-wanda-maximoff',
+    img: 'https://terrigen-cdn-dev.marvel.com/content/prod/1x/012scw_ons_crd_02.jpg',
+    isActive: false
+  },
+  {
+    hero: 'HULK',
+    name: 'Robert Bruce Banner',
+    link: 'http://marvel.com/characters/25/hulk',
+    img: 'https://terrigen-cdn-dev.marvel.com/content/prod/1x/006hbb_ons_crd_03.jpg',
+    isActive: false
+  },
+  {
+    hero: 'Black Widow',
+    name: "Natalia 'Natasha' Alianovna Romanova",
+    link: 'http://marvel.com/characters/6/black_widow',
+    img: 'https://terrigen-cdn-dev.marvel.com/content/prod/1x/011blw_ons_crd_04.jpg',
+    isActive: false
+  },
+  {
+    hero: 'Doctor Strange',
+    name: 'Stephen Strange',
+    link: 'https://www.marvel.com/characters/doctor-strange-stephen-strange',
+    img: 'https://terrigen-cdn-dev.marvel.com/content/prod/1x/009drs_ons_crd_02.jpg',
+    isActive: false
+  },
+  {
+    hero: 'Captain America',
+    name: 'Steven Rogers',
+    link: 'http://marvel.com/characters/8/captain_america',
+    img: 'https://terrigen-cdn-dev.marvel.com/content/prod/1x/003cap_ons_crd_03.jpg',
+    isActive: false
+  },
+  {
+    hero: 'Iron Man',
+    name: "Anthony Edward 'Tony' Stark",
+    link: 'http://marvel.com/characters/29/iron_man',
+    img: 'https://terrigen-cdn-dev.marvel.com/content/prod/1x/002irm_ons_crd_03.jpg',
+    isActive: false
+  },
+  {
+    hero: 'THOR',
+    name: 'Thor Odinson',
+    link: 'http://marvel.com/characters/60/thor',
+    img: 'https://terrigen-cdn-dev.marvel.com/content/prod/1x/004tho_ons_crd_03.jpg',
+    isActive: false
+  },
+];
