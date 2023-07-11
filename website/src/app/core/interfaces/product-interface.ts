eba9901970b46269b0b52f65911e1e660cd571c3
Nikki <nikki.asteinza@gmail.com>
2023-07-11 21:37:30 +0200

    Update data interfaces to match an API Data

--- website/src/app/core/interfaces/product-interface.ts
+++ website/src/app/core/interfaces/product-interface.ts
@@ -1,13 +1,40 @@
 export interface ProductI{
-    createdAt?: string; // Esto es opcional
-    name:string;
-    // color:string;
-    price:number;
-    description:string;
-    image:string;
-    id:string;
-    stock:number;
-    // materials: ProductMaterial;
+    id: number;
+    category: number;
+    title: string;
+    description: string;
+    tags: string;
+    image: string;
+    prep_time: string;
+    cook_time: string;
+    num_of_ratings: string;
+    rating: string;
+    difficulty: string;
+    ingred1: string;
+    ingred2: string;
+    ingred3: string;
+    ingred4: string;
+    ingred5: string;
+    ingred6: string;
+    ingred7: string;
+    ingred8: string;
+    ingred9: string;
+    ingred10: string;
+    ingred11: string;
+    ingred12: string;
+    instruct1: string;
+    instruct2: string;
+    instruct3: string;
+    instruct4: string;
+    instruct5: string;
+    instruct6: string;
+    instruct7: string | null;
+    instruct8: string | null;
+    instruct9: string | null;
+    instruct10: string | null;
+    instruct11: string | null;
+    instruct12: string | null;
+    tips: string;
 }
 
 // export enum ProductMaterial{
@@ -15,13 +42,13 @@ export interface ProductI{
 // }
 
 // export type ProductMaterial = string| number;
-export type ProductMaterial = 'Steel'
-| "Metal"
-| "Cotton"
-| "Fresh"
-| "Concrete"
-| "Gratine"
-| "Frozen"
-| "Rubber"
-| "Wooden"
-| "Soft";
+// export type ProductMaterial = 'Steel'
+// | "Metal"
+// | "Cotton"
+// | "Fresh"
+// | "Concrete"
+// | "Gratine"
+// | "Frozen"
+// | "Rubber"
+// | "Wooden"
+// | "Soft";
