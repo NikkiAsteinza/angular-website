bdebb36289952cf0b83a5c978b168c9dcab3ec6f
Nikki A.A <nikki.asteinza@gmail.com>
2023-07-13 12:44:45 +0200

    Shared sections and core buttons

---
+++ website/src/app/shared/components/page-sections/one-columns-section/one-columns-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-one-columns-section',
+  templateUrl: './one-columns-section.component.html',
+  styleUrls: ['./one-columns-section.component.scss']
+})
+export class OneColumnsSectionComponent {
+
+}
---
+++ website/src/app/shared/components/page-sections/two-columns-section/two-columns-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-two-columns-section',
+  templateUrl: './two-columns-section.component.html',
+  styleUrls: ['./two-columns-section.component.scss']
+})
+export class TwoColumnsSectionComponent {
+
+}
